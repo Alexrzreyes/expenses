d2ca38090ae51abad9504f9d550f52e9f0f2d444
Alexisrzreyes <alexis.rz.reyes@gmail.com>
2026-05-23 17:58:43 -0600

    featue: add deploy

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Use relative paths for assets so that the build works in subdirectories like GitHub Pages
+  base: './'
+});
